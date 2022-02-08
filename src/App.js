29b7399cbd2a0bc33f4161f86f9a798aca2ff657
bhaktibuana <bhaktibuana19@gmail.com>
2022-02-08 17:55:25 +0700

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { apiURL } from "./api/apiURL";
+import NotFound from "./pages/NotFound/NotFound";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} exact />
+          <Route path="*" element={<NotFound />} exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Homepage.css";
+
+const Homepage = () => {
+  const [productName, setProductName] = useState("All Products");
+  const [sidebarStatus, setSidebarStatus] = useState(false);
+  const [getProducts, setGetProducts] = useState({});
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [filterItem, setFilterItem] = useState("All");
+
+  const storeObj = {
+    page: "homepage",
+    productName,
+    setProductName,
+    sidebarStatus,
+    setSidebarStatus,
+    getProducts,
+    currentPageNumber,
+    setCurrentPageNumber,
+    filterItem,
+    setFilterItem,
+  };
+
+  return (
+    <>
+      <div className="homepage-container">
+        <Navbar storeObj={storeObj} />
+      </div>
+
+      <div className="homepage-footer"></div>
+    </>
+  );
+};
+
+export default Homepage;
